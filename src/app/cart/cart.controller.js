bc1e15ec3825f8871d1cf4aa6695b11e722ac990
maodandan <dandan.mao@shuyun.com>
2015-07-07 08:59:27 +0800

    Merge branch 'master' of E:\brand guider 2\2.1\daogou with conflicts.

--- src/app/cart/cart.controller.js
+++ src/app/cart/cart.controller.js
@@ -1,234 +1,215 @@
 'use strict';
 
-var cart = angular.module('cart',['ionic']);
-cart.controller('cartCtrl',['$scope', '$log', '$http','$state','URLPort','$stateParams','daogouAPI','$rootScope', function($scope,$log,$http,$state,URLPort,$stateParams,daogouAPI,$rootScope){
+var cart = angular.module('cart', ['ionic']);
+cart.controller('cartCtrl', ['$scope', '$log', '$http', '$state', 'URLPort', '$stateParams', 'daogouAPI', '$rootScope', function ($scope, $log, $http, $state, URLPort, $stateParams, daogouAPI, $rootScope) {
+  var URLPort = URLPort();
+  $scope.cartProductListData = [];
+  $scope.hasMoreOrder = true;
+  var pageindex = 1;
+  var pagesize = 5;
+  var userid = $stateParams.userid;
+  var brandid = $stateParams.brandid;
 
-
-//==============================阅完可删除,若不删,留作纪念,我也不反对线====================================
-	//这个切换其实是2个页面 不是页面内切换的
-	//一个是购物车页cart   应该是订单列表  order → order-list
-//==============================阅完可删除,若不删,留作纪念,我也不反对线====================================
-    //先检测是否登录，若登陆了则获取用户信息，未登录进入登录界面
-    daogouAPI.isLogin(
-        function(data){
-            console.log("我是order登录检查登录了")
-            console.log(data)
-            //获取订单信息
-            cartProductListFunc();
-        }
-        ,function(data){
-            console.log("我是order登录检查没登录")
-            $scope.login=true;
-        }
-    );
-    //检测登录结束
-    //获取购物车信息开始
-    var URLPort = URLPort();
-    $scope.cartProductListData = [];
-    var pageindex = 1;
-    var pagesize = 5;
-    $scope.hasMoreOrder = true;
-	function cartProductListFunc(){
-		daogouAPI.shopcart({
-			userid:$rootScope.USERINFO.id,
-			brand_id:$rootScope.BRANDID,
-			page:pageindex,
-			per_page:pagesize
-		},function(data, status, headers, config){
-			console.log(["查询导购商品列表成功",data]);
-			console.log(["hasMoreOrder",$scope.hasMoreOrder])
-			console.log(["pageindex",pageindex]);
-			for(var i in data){
-				data[i].seleted = false;
-			}
-			$scope.cartProductListData = $scope.cartProductListData.concat(data);
-                console.log(['datadatadatadatadatadatadata',data]);
-
-
-            //获得当前购物车的物品属性
-            var cartArr=$scope.cartProductListData[0].sku_properties_name.split(';');
-            console.log(['cartArr',cartArr])
-            console.log(['$scope.cartProductListData[0].sku_properties_name',$scope.cartProductListData[0].sku_properties_name])
-            $scope.cartProductListData.specification=[];
-            for(var idx in cartArr){
-                $scope.cartProductListData.specification[idx] = {};
-                $scope.cartProductListData.specification[idx].key = "";
-                $scope.cartProductListData.specification[idx].val = "";
-                console.log(["SAAAAAAAAAAAAAAAA",$scope.cartProductListData.specification])
-            }
-            for(var id in data){//cartProductListData的数量
-            var flag = true;
-            var cartArr = $scope.cartProductListData[id].sku_properties_name.split(';');//cartArr.length参数种类
-             console.log(['$scope.cartProductListData[id].sku_properties_name',$scope.cartProductListData[id].sku_properties_name])
-            for(var tz in cartArr){//一个规格种类一个规格种类来
-                var cartendArr = cartArr[tz].split(':');//取每个规格的规格名和规格值
-                $scope.cartProductListData.specification[tz].val = cartendArr[cartendArr.length-1];//规格值
-                $log.debug(['$scope.cartProductListData.specification',$scope.cartProductListData.specification]);
-                $scope.cartProductListData.specification[tz].array = $scope.cartProductListData.specification[tz].val.split(" ");
-                $scope.cartProductListData.specification[tz].array.splice($scope.cartProductListData.specification[tz].array.length-1,1);
-                $scope.cartProductListData.specification[tz].key = cartendArr[cartendArr.length-2];//规格名
-            }
-            console.log(["SBBBBBBBBBBBBB",$scope.cartProductListData.specification])
-        };
-
-
-		    console.log(["data.length",data.length])
-		    if(data.length>=pagesize){
-		    	pageindex++;
-		    	console.log(["pageindex+++++++",pageindex])
-		    }else{
-		    	$scope.hasMoreOrder = false;
-		    	console.log(["hasMoreOrder",$scope.hasMoreOrder])
-		    }
-		    $scope.$broadcast('scroll.infiniteScrollComplete');
-		},function(data, status, headers, config){
-			console.log(["查询导购商品列表失败",data]);
-		});
-	}
-    //回调登录开始
-    $scope.loginsuccess=function(data){
-      console.log(["cart的回调", data]);
-      $(".redPoint").show();
-      $scope.login=false;
-      //获取订单信息
-      cartProductListFunc();
-    }
-    $scope.loginerror=function(data){
-
-    }
-    //回调登录结束
-
-	$scope.loadMoreData = function(){
-		console.log(["loadMoreData"]);
-		cartProductListFunc();
-	}
-
-	$scope.$on('$stateChangeSuccess', function() {
-		 if(pageindex>2){
-	   		$scope.loadMoreData();
-	   	 }
-	 });
-
-	$http.get(URLPort+"/brands/"+$rootScope.BRANDID)
-	.success(function(data){
-		console.log(["获取品牌信息成功",data]);
-		$scope.brandData = data;
-	})
-	.error(function(data){
-		console.log(["获取品牌信息失败",data])
-	})
-
-	
-//点击+ - 增减商品数
-	$scope.delNum = function(index){
-		$scope.cartProductListData[index].num--;
-		$scope.cartProductListData[index].num = $scope.cartProductListData[index].num>0?$scope.cartProductListData[index].num:0;
-	}
-	$scope.addNum = function(index){
-		$scope.cartProductListData[index].num++;
-	}
-
-	
-   $scope.totalNum = 0;//购物车中选中商品总量
-   $scope.totalFee = 0;//合计价格
-   //通过点击选中圆圈选中
-    $scope.changeCheck = function(index){
-    	console.log(["indexindexindex",index])
-    	if($scope.cartProductListData[index].seleted === true){
-    		$scope.cartProductListData[index].seleted = false;
-    	}else{
-    		$scope.cartProductListData[index].seleted = true;
-    	}
-    	
-    	
-    	$scope.totalFee = 0;//合计价格
-    	$scope.totalNum = 0;
-    	$scope.ids = "";//选中商品id集合
-    	
-    	for(var i in $scope.cartProductListData){
-    		if($scope.cartProductListData[i].seleted === true){
-    			$scope.totalFee += parseFloat($scope.cartProductListData[i].total_fee);
-    			$scope.totalNum++;
-    			$scope.ids +=$scope.cartProductListData[i].id+",";
-    		}
-    		
-    	}
-    	$scope.ids = $scope.ids.substr(0,$scope.ids.length-1);
-    	console.log(["$scope.ids",$scope.ids])
- 		
- 		
-    }
-
+  /**
+   * 获取购物车列表
+   */
+  function cartProductListFunc() {
     $scope.Allseleted = false;
-     //全选全不选
-    $scope.changeAll = function(){
-    	console.log(["changeAll"]);
-    	//选中与不选中 选中图标的转换
-    	if($scope.Allseleted === false){
-    		console.log("biangreen")
-    		$scope.Allseleted = true;
-    		for(var i in $scope.cartProductListData){
-    			$scope.cartProductListData[i].seleted = true;
-    			console.log(["$scope.cartProductListData[i].seleted",$scope.cartProductListData[i].seleted]);
-    		}
-    	}else{
-    		console.log("biangray")
-    		$scope.Allseleted = false;
-    		for(var i in $scope.cartProductListData){
-    		    $scope.cartProductListData[i].seleted = false;
-    		}
-    	}
-    	//合计价格
-    	 $scope.totalFee = 0;
-    	 $scope.totalNum = 0;
-    	 $scope.ids = "";//选中商品id集合
-    	for(var i in $scope.cartProductListData){
-    		if($scope.cartProductListData[i].seleted === true){
-    			$scope.totalFee += parseFloat($scope.cartProductListData[i].total_fee);
-    			$scope.totalNum++;
-    			$scope.ids +=$scope.cartProductListData[i].id+",";
-    		}
-    		
-    	}
-    	$scope.ids = $scope.ids.substr(0,$scope.ids.length-1);
-    	console.log(["$scope.ids",$scope.ids])
-    }
-    $scope.deleteCartProduct = function(){
-    	console.log(["删除商品$scope.ids",$scope.ids]);
-    	daogouAPI.deleteCartProduct({
-			userid:userid,
-			ids:$scope.ids
-		},function(data, status, headers, config){
-			console.log(["删除购物车商品成功",data]);
-            $scope.cartProductListData = [];
-            pageindex = 1;
-            pagesize = 5;
-            $scope.hasMoreOrder = true;
-            cartProductListFunc();
+    daogouAPI.shopcart({
+      userid: userid,
+      brand_id: brandid,
+      page: pageindex,
+      per_page: pagesize
+    }, function (data, status, headers, config) {
+      angular.forEach(data, function (item, index) {
+        item.seleted = false;
+        item.pics = item.pic_path.indexOf(",") > 0 ? item.pic_path.split(",") : [item.pic_path];
+        item.specification = [];
+        item.item = 1;
+        var cartArr = item.sku_properties_name.split(';');//cartArr.length参数种类
+        for (var tz in cartArr) {//一个规格种类一个规格种类来
+          var cartendArr = cartArr[tz].split(':');//取每个规格的规格名和规格值
+          item.specification[tz] = {val: "", array: "", key: ""};
+          item.specification[tz].val = cartendArr[cartendArr.length - 1];//规格值
+          item.specification[tz].array = item.specification[tz].val.split(" ");
+          item.specification[tz].array.splice(item.specification[tz].array.length - 1, 1);
+          item.specification[tz].key = cartendArr[cartendArr.length - 2];//规格名
+        }
+      });
+      $scope.cartProductListData = $scope.cartProductListData.concat(data);
+      if (data.length >= pagesize) {
+        pageindex++;
+      } else {
+        $scope.hasMoreOrder = false;
+        console.log(["hasMoreOrder", $scope.hasMoreOrder])
+      }
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    }, function (data, status, headers, config) {
+      console.log(["查询导购商品列表失败", data]);
+    });
+  }
 
-		},function(data, status, headers, config){
-			console.log(["删除购物车商品失败",data]);
-		});
+  cartProductListFunc();
+  /**
+   * 加载更多
+   */
+  $scope.loadMoreData = function () {
+    console.log(["loadMoreData"]);
+    cartProductListFunc();
+  };
+  /**
+   * 监测广播，加载更多
+   */
+  $scope.$on('$stateChangeSuccess', function () {
+    if (pageindex > 2) {
+      $scope.loadMoreData();
     }
+  });
+  /**
+   * 加载品牌信息
+   */
+  $http.get(URLPort + "/brands/" + brandid)
+    .success(function (data) {
+      $scope.brandData = data;
+    })
+    .error(function (data) {
+    });
+  $scope.totalNum = 0;//购物车中选中商品总量
+  $scope.totalFee = 0;//合计价格
+  /**
+   * 点击-减商品数
+   * @param index
+   */
+  $scope.delNum = function (item) {
+    if (item.num > 1) {
+      $scope.ids.replace(item.id + ",", "");
+      $scope.totalNum--;
+      $scope.totalFee -= parseFloat(item.total_fee);
+      item.num--;
+      item.num = item.num > 0 ? item.num : 0;
+    } else {
+      alert("不能再少了")
+    }
+  };
+  /**
+   * 点击+增商品数
+   * @param index
+   */
+  $scope.addNum = function (item) {
+    item.num++;
+    $scope.totalNum++;
+    $scope.totalFee += parseFloat(item.total_fee);
+    if (item.num > 0) {
+      $scope.ids += item.id + ",";
+      item.seleted = true;
+      var isAll = true;
+      $scope.cartProductListData.filter(function (item) {
+        if (!item.seleted) {
+          isAll = false;
+        }
+      });
+      $scope.Allseleted = isAll;
+    }
+  };
 
+  /**
+   *   通过点击选中圆圈选中
+   */
+  $scope.changeCheck = function (item) {
+    item.seleted = !item.seleted;
+    if (item.seleted) {
+      item.num = 1;
+    }
+    if (item.seleted) {
+      $scope.totalFee += parseFloat(item.total_fee);
+      $scope.totalNum += item.num;
+      $scope.ids += item.id + ",";
+    }
+    else {
+      $scope.totalNum -= item.num;
+      $scope.totalFee -= parseFloat(item.total_fee);
+      $scope.ids.replace(item.id + ",", "");
+    }
+    var isAll = true;
+    $scope.cartProductListData.filter(function (item) {
+      if (!item.seleted) {
+        isAll = false;
+      }
+    });
+    $scope.Allseleted = isAll;
+  };
+  /**
+   *   全选全不选
+   */
+  $scope.Allseleted = false;
+  $scope.changeAll = function () {
+    $scope.Allseleted = !$scope.Allseleted;
+    $scope.totalFee = 0;//合计价格
+    $scope.totalNum = 0;
+    $scope.ids = "";
+    angular.forEach($scope.cartProductListData, function (item, index) {
+      item.seleted = $scope.Allseleted;
+      if (item.num > 0 && item.seleted) {
+        $scope.totalFee += parseFloat(item.total_fee);
+        $scope.totalNum += item.num;
+        $scope.ids += item.id + ",";
+        $scope.ids = $scope.ids.substr(0, $scope.ids.length - 1);
+      }
+    });
+
+  };
+
+  /**
+   *   合计价格
+   */
+  $scope.totalFee = 0;
+  $scope.totalNum = 0;
+  $scope.ids = "";//选中商品id集合
+  angular.forEach($scope.cartProductListData, function (item, index) {
+    if (item.seleted) {
+      $scope.totalFee += parseFloat(item.total_fee);
+      $scope.totalNum++;
+      $scope.ids += item.id + ",";
+    }
+  });
+  $scope.ids = $scope.ids.substr(0, $scope.ids.length - 1);
+
+  /**
+   * 删除商品
+   */
+  $scope.deleteCartProduct = function () {
+    daogouAPI.deleteCartProduct({
+      userid: userid,
+      ids: $scope.ids
+    }, function (data, status, headers, config) {
+      $scope.cartProductListData = [];
+      pageindex = 1;
+      pagesize = 5;
+      $scope.hasMoreOrder = true;
+      cartProductListFunc();
+    }, function (data, status, headers, config) {
+      console.log(["删除购物车商品失败", data]);
+    });
+  };
+
+  $scope.edithandle = true;
+  $scope.finishhandle = false;
+  $scope.edit = function () {
+    $scope.edithandle = false;
+    $scope.finishhandle = true;
+
+  };
+  $scope.finish = function () {
     $scope.edithandle = true;
     $scope.finishhandle = false;
-    console.log(["$scope.edit",$scope.edit]);
-    console.log(["$scope.finish",$scope.finish]);
-    $scope.edit = function(){
-    	console.log(["编辑"]);
-    	$scope.edithandle = false;
-    	$scope.finishhandle = true;
-    }
-    $scope.finish = function(){
-    	console.log(["完成啦"])
-    	$scope.edithandle = true;
-    	$scope.finishhandle = false;
-    }
-    //购物车 订单列表切换
-    $scope.goOrderList = function(){
-        console.log(["goOrderList  $rootScope.USERINFO.id",$rootScope.USERINFO.id]);
-        $state.go("orderList",{"userid": $rootScope.USERINFO.id});
-    }
-    
-}]);
+  };
+
+
+  /**
+   *   购物车 订单列表切换
+   */
+  $scope.goOrderList = function () {
+    $state.go("orderList", {"userid": $stateParams.userid});
+  }
+}])
+;
